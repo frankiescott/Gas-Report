ee4474226ee1e48c84de3023a252438c3c186728
Frankie <email@frankiescott.net>
2017-02-19 18:39:11 -0500

    Update GasReport.java

--- GasReport.java
+++ GasReport.java
@@ -6,19 +6,19 @@ import java.io.File;
 public class GasReport {
 
 	public static void main(String[] args) throws java.io.IOException {
-        JFileChooser jfc = new JFileChooser("C:\\");
-        jfc.setDialogTitle("Gas Report: Browse Files");
-        jfc.setAcceptAllFileFilterUsed(false);
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Excel Spreadsheets", "xlsx");
-        jfc.addChoosableFileFilter(filter);
+        	JFileChooser jfc = new JFileChooser("C:\\");
+        	jfc.setDialogTitle("Gas Report: Browse Files");
+        	jfc.setAcceptAllFileFilterUsed(false);
+       		FileNameExtensionFilter filter = new FileNameExtensionFilter("Excel Spreadsheets", "xlsx");
+        	jfc.addChoosableFileFilter(filter);
         
-        int returnValue = jfc.showDialog(null, "Submit");
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-	        File selectedFile = jfc.getSelectedFile();
-		    String filepath = selectedFile.getAbsolutePath();
+        	int returnValue = jfc.showDialog(null, "Submit");
+        	if (returnValue == JFileChooser.APPROVE_OPTION) {
+	        	File selectedFile = jfc.getSelectedFile();
+			String filepath = selectedFile.getAbsolutePath();
 
-            ProcessBuilder pb = new ProcessBuilder("python", "C:\\Python36\\gasreport.py", filepath);
-            Process p = pb.start();
+            		ProcessBuilder pb = new ProcessBuilder("python", "C:\\Python36\\gasreport.py", filepath);
+            		Process p = pb.start();
 		}
 	}
 }  
